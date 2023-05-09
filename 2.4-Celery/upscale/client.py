6182a489d0ffed7bd94d3fba1eb1980a11690dc9
Matthewous <moguch98@mail.ru>
2023-05-09 16:52:35 +0300

    final commit

--- 2.4-Celery/upscale/client.py
+++ 2.4-Celery/upscale/client.py
@@ -5,14 +5,11 @@ import time
 response = requests.post("http://127.0.0.1:5000/upscale", files={"image_1": open("lama_300px.png", "rb")})
 
 response_data = response.json()
-print(response_data)
 task_id = response_data.get("task_id")
-print(task_id)
 
 while True:
     resp = requests.get(f"http://127.0.0.1:5000/tasks/{task_id}")
-    print(resp.json())
-    time.sleep(25)
+    time.sleep(10)
     if resp.json()["status"] == "SUCCESS":
         print("ok")
         break
